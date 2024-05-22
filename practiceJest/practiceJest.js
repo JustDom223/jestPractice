19c1de2edaa53468773ab5d51927657e7ef1bbf1
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-05-22 20:18:14 +0200

    Working on the calculator object I have created 5 tests and completed them one after another. This has been done for the add and subtract functions

--- practiceJest/practiceJest.js
+++ practiceJest/practiceJest.js
@@ -3,5 +3,32 @@ export function capitalize(string) {
 }
 
 export function reverseString(string) {
-    return 'gnirtS';
+    const stringArray = string.split('');
+    let reverseStringArray = [];
+    stringArray.forEach((element) => {
+        reverseStringArray.unshift(element);
+    });
+    return reverseStringArray.join('');
 }
+
+export const calculator = {
+    add: function (...args) {
+        const flatArgs = args.flat(Infinity);
+        const filteredNumbers = flatArgs.filter((element) => !isNaN(element));
+        return filteredNumbers.reduce(
+            (accumulator, current) => accumulator + current,
+            0,
+        );
+    },
+    subtract: function (...args) {
+        const flatArgs = args.flat(Infinity);
+        const filteredNumbers = flatArgs.filter((element) => !isNaN(element));
+        console.log(filteredNumbers);
+        let accumulator = filteredNumbers[0];
+
+        for (let i = 1; i < filteredNumbers.length; ++i) {
+            accumulator = accumulator - filteredNumbers[i];
+        }
+        return accumulator;
+    },
+};
--- practiceJest/practiceJest.test.js
+++ practiceJest/practiceJest.test.js
@@ -1,6 +1,7 @@
-import { capitalize, reverseString } from './practiceJest';
+import { calculator, capitalize, reverseString } from './practiceJest';
 
-test('Take string and return it with the first letter capitalized', () => {
+//Tests for capitalize function
+test('capitalize: take string and return it with the first letter capitalized', () => {
     expect(capitalize('string')).toBe('String');
 });
 
@@ -24,12 +25,68 @@ test('capitalize: does not change numbers or symbols', () => {
     expect(capitalize('123!')).toBe('123!');
 });
 
-test('capitalize: capitalizes each word in a sentence', () => {
+test('capitalize: Does not capitalize each word in a sentence', () => {
     expect(capitalize('hello world, how are you?')).toBe(
         'Hello world, how are you?',
     );
 });
 
-test('Take string and return it reversed', () => {
+//Tests for a function that reverses the string
+test('reverseString: take a string and return it reversed', () => {
     expect(reverseString('String')).toBe('gnirtS');
+});
+
+test('reverseString: handles empty string', () => {
+    expect(reverseString('')).toBe('');
+});
+
+test('reverseString: reverses a single letter', () => {
+    expect(reverseString('A')).toBe('A');
+});
+
+test('reverseString: mixed sentence with numbers and symbols', () => {
+    expect(
+        reverseString(
+            'The address is 31, Big Toe Street, Vic, 3315, Australia',
+        ),
+    ).toBe('ailartsuA ,5133 ,ciV ,teertS eoT giB ,13 si sserdda ehT');
+});
+
+// Tests for calculator object
+const numberArray = [1, 2, 3, 4, 5, 'A', 7, 8, 9, 10];
+const backwardsNumberArray = [10, 9, 8, 7, 'A', 5, 4, 3, 2, 1];
+const backwardsNestedNumberArray = [10, 9, 8, 7, 'A', 5, 4, 3, 2, 1];
+const nestedNumberArray = [1, [2, [3, 4], 5, 'A', [7, 8], 9], 10];
+//Add
+test('Calculator: add 2 numbers together', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+});
+test('Calculator: add 3 numbers together', () => {
+    expect(calculator.add(1, 2, 3)).toBe(6);
+});
+test('Calculator: add 9 numbers together', () => {
+    expect(calculator.add(1, 2, 3, 4, 5, 6, 7, 8, 9)).toBe(45);
+});
+test('Calculator: add numbers but deal with an accidental letter', () => {
+    expect(calculator.add(numberArray)).toBe(49);
+});
+test('Calculator: Testing nested arrays', () => {
+    expect(calculator.add(nestedNumberArray)).toBe(49);
+});
+
+//Subtract
+test('Calculator: subtract 1 number from another', () => {
+    expect(calculator.subtract(2, 1)).toBe(1);
+});
+test('Calculator: subtract 2 numbers from the first', () => {
+    expect(calculator.subtract(3, 2, 1)).toBe(0);
+});
+test('Calculator: add 9 numbers together', () => {
+    expect(calculator.subtract(100, 1, 2, 3, 4, 5, 6, 7, 8, 9)).toBe(55);
+});
+test('Calculator: subtract numbers but deal with an accidental letter', () => {
+    expect(calculator.subtract(...backwardsNumberArray)).toBe(-29);
+});
+test('Calculator: Testing nested arrays', () => {
+    expect(calculator.subtract(backwardsNestedNumberArray)).toBe(-29);
 });
